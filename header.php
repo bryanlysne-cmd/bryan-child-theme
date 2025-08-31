ec7d4a6faf33a92931de9a3c7ffb8e5f98fb9493
bryanlysne-cmd <bryan.lysne@gmail.com>
2025-08-30 22:08:41 -0700

    Update header.php with mobile-friendly hamburger header and navigation toggle

---
+++ header.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Brand</a>
+  </div>
+  <div class="collapse navbar-collapse" id="navbar-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</nav>
